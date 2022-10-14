1ac61ec878cb4f6101f36b9597ff049f134e5a43
jizzy19 <edochiemoge@gmail.com>
2022-10-13 19:31:44 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main-entry point for the program
+ *
+ * Return:0 if no error, return non if errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
