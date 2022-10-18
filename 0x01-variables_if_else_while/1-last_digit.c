d13bc89a252c0e98e0e0501ab349d0c1156c9f23
jizzy19 <edochiemoge@gmail.com>
2022-10-17 17:09:26 -0700

    String error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (digit < 6 && digit != 0)
 	{
-		("Last digit of %d is %d is and is less than 6 and not 0\n", n, digit);
+		("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
 	return (0);
 	}
